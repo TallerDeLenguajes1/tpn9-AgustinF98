d153db2a8bd9bb48707fbb8be864787f9b6399bd
Agustin <agus_gymnas2009@hotmail.com>
2020-07-09 21:29:18 -0300

    Primer Commit

---
+++ TP9/Helpers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Globalization;
+
+namespace Helpers
+{
+    static class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion(string Ruta)
+        {
+            FileStream Archivo = new FileStream(Ruta + @"\destino.dat", FileMode.Create);
+            BinaryWriter ArchivoBinario = new BinaryWriter(Archivo);
+            ArchivoBinario.Close();
+            Archivo.Close();
+        }
+
+        public static string LeerConfiguracion(string Ruta)
+        {
+            FileStream Archivo = new FileStream(Ruta + @"\destino.dat", FileMode.OpenOrCreate, FileAccess.Read);
+            BinaryReader LeerArchivoBinario = new BinaryReader(Archivo);
+            string Datos = LeerArchivoBinario.ReadString();
+            LeerArchivoBinario.Close();
+            Archivo.Close();
+
+            return Datos;
+        }
+    }
+
+    static class ConversorDeMorse
+    {
+        public static Dictionary<string, string> ClavesMorse = new Dictionary<string, string>()
+        {
+            {"A", ".-"}, {"B", "-..."}, {"C", "-.-."}, {"D", "-.."},
+            {"E", "."}, {"F", "..-."}, {"G", "--."}, {"H", "...."},
+            {"I", ".."}, {"J", ".---"}, {"K", "-.-"}, {"L", ".-.."},
+            {"M", "--"}, {"N", "-."}, {"O", "---"}, {"P", ".--."},
+            {"Q", "--.-"}, {"R", ".-."}, {"S", "..."}, {"T", "-"},
+            {"U", "..-"}, {"V", "...-"}, {"W", ".--"}, {"X", "-..-"},
+            {"Y", "-.--"}, {" ", "/"}, {"Z", "--.."}, {"1", ".----"}, {"2", "..---"},
+            {"3", "...--"}, {"4", "....-"}, {"5", "....."}, {"6", "-...."},
+            {"7", "--..."}, {"8", "---.."}, {"9", "----."}, {"0", "-----"},
+        };
+
+        /*public static string MorseATexto(string TextoMorse)
+        {
+            string textoTraducido;
+
+            foreach(string caracter in TextoMorse)
+            {
+
+            }
+            
+            return textoTraducido;
+        }*/
+
+        public static string TextoAMorse(string TextoCastellano)
+        {
+            string textoTraducido = "";
+
+            foreach (char caracter in TextoCastellano)
+            {
+                foreach(KeyValuePair<string, string> valor in ClavesMorse)
+                {
+                    if (valor.Key == caracter.ToString())
+                    {
+                        textoTraducido = valor.Value;
+                    }
+                }
+            }
+
+            return textoTraducido;
+        }
+    }
+}
---
+++ TP9/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Helpers;
+
+namespace TP9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Ruta = @"C:\Repogit\tp9\tpn9-AgustinF98\TP9";
+            
+            List<string> ListadoArchivos;
+
+            if (File.Exists("destino.dat"))
+            {
+                //Console.WriteLine("El archivo 'destino.dat' no existe. Se creara a continuacion.\n");
+                SoporteParaConfiguracion.LeerConfiguracion(Ruta);
+            }
+            else
+            {
+                SoporteParaConfiguracion.CrearArchivoDeConfiguracion(Ruta);
+                SoporteParaConfiguracion.LeerConfiguracion(Ruta);
+            }
+
+            string Destino = Ruta + @"\destino.dat";
+            string DirectorioRaiz = @"C:\Repogit\tp9\tpn9-AgustinF98\TP9_1\bin\Debug";
+            ListadoArchivos = Directory.GetFiles(DirectorioRaiz).ToList();
+            Console.WriteLine("Listado de archivos: ");
+
+            foreach (string archivos in ListadoArchivos)
+            {
+                Console.WriteLine(archivos);
+                if(Path.GetExtension(archivos) == ".mp3" || Path.GetExtension(archivos) == ".txt")
+                {
+                    if (File.Exists(Destino))
+                    {
+                        File.Delete(Destino);
+                    }
+                    File.Move(archivos, Destino);
+                }
+            }
+
+
+            //-------------- PUNTO 2 --------------
+
+            string TextoMorse, TextoCastellano;
+            string carpetaMorse = Ruta + @"\Morse";
+
+            Console.WriteLine("Ingrese un texto para traducirla a codigo morse: ");
+            TextoCastellano = Console.ReadLine();
+            TextoMorse = ConversorDeMorse.TextoAMorse(TextoCastellano);
+
+            if (!Directory.Exists(carpetaMorse))
+            {
+                Directory.CreateDirectory(carpetaMorse);
+            }
+        }
+    }
+}
